12e4e7b07e63fdf8e3f33b83c842424851f41efd
Ruega <purelbatako@gmail.com>
2023-06-28 05:37:45 +0000

    Revert "sdm660-common: Derpfest OS"
    This reverts commit f844150b1e3d3cefa4373cc49352a6b09e46bbdd.

--- sdm660.mk
+++ sdm660.mk
@@ -1,8 +1,6 @@
 #
 # Copyright (C) 2020 The LineageOS Project
 #
-# Copyright (C) 2021 DerpFest
-#
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -307,7 +305,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-derp
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
